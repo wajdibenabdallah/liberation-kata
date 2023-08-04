1bf32d34823e0649c9d6532c699c37245253bbd2
wajdi.ben-abdallah.externe <wajdibabdallah@gmail.com>
2023-08-04 14:27:28 +0200

    define calculatePrice

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -9,4 +9,9 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
+
+  @Get()
+  calculatePrice(): number {
+    return this.appService.calculatePrice();
+  }
 }
